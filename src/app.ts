f99af2978cd53b72179358da86b2d156da6323df
Mohammad Noughabi <mohammadnoughabi@gmail.com>
2026-09-19 03:48:12 +0330

    initialize project and add README

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { applySecurityMiddleware } from '@common/middlewares/security.js';
 import { errorHandler } from '@common/middlewares/errorHandler.js';
 import { setupSwagger } from '@config/swagger.js';
 import router from '@routes/index.js';
@@ -8,7 +7,6 @@ export const app = express();
 
 app.use(express.json());
 
-applySecurityMiddleware(app);
 setupSwagger(app);
 
 app.use('/api', router);
